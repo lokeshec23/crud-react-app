060331bb1a042601611500befa75d40bfa11d95c
lokeshec23 <lokeshs.ece2019@dce.edu.in>
2023-11-12 20:28:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <h1>CRUD APP With React</h1>
+      <div className='flex-row'>
+        <div className='flex-large'>
+          <h2>Add user</h2>
+        </div>
+        <div className='flex-large'>
+          <h2>view users</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
